206c64a9575a7ad1f7ff254797a9d1ca77ee0051
土豆 <42114446+tudou566@users.noreply.github.com>
2022-04-28 01:36:36 +0800

    Update GitTest.java

--- src/main/java/com/cttq/GitTest.java
+++ src/main/java/com/cttq/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hotfix conf");
         System.out.println("push conf");
+        System.out.println("pull conf");
     }
 }
